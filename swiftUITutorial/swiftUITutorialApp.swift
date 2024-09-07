985b05529c33949170d8760540cac2eb4bc8b4a8
bunabloom <kimrinoffdev@gmail.com>
2024-09-07 19:37:13 +0900

    Initial Commit

---
+++ swiftUITutorial/swiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUITutorialApp.swift
+//  swiftUITutorial
+//
+//  Created by KimRin on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
