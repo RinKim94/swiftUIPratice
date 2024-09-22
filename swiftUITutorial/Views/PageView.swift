e2d5945e3c63e8d2555d7008e003382a9b0669be
bunabloom <kimrinoffdev@gmail.com>
2024-09-22 15:45:14 +0900

    Interfacing with UIKit

---
+++ swiftUITutorial/Views/PageView.swift
@@ -0,0 +1,27 @@
+//
+//  PageView.swift
+//  swiftUITutorial
+//
+//  Created by KimRin on 9/22/24.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 0
+    
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
+        .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
+
+#Preview {
+    PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+}
